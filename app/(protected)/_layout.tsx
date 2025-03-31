d9c4b0e3e7e8918e42d0bc77acbef1090671e71a
Richard Westmoreland <rawestmoreland@gmail.com>
2025-03-31 21:44:56 +0100

    fix: update dependency in layout effect to include selected session for improved functionality

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -74,7 +74,7 @@ export default function TabLayout() {
         keyboardDidHideListener.remove();
       };
     }
-  }, [activeSesh]);
+  }, [activeSesh, selectedSesh]);
 
   useEffect(() => {
     if (selectedSesh) {
